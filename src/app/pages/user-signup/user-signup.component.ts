e07931125f33a31a299df45c2e783c0a3c41e57b
Aileen Olitoquit <108129228+Puffssss1@users.noreply.github.com>
2024-02-13 03:56:24 +0800

    MED-2-page1

--- src/app/pages/user-signup/user-signup.component.ts
+++ src/app/pages/user-signup/user-signup.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-user-signup',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatIconModule],
   templateUrl: './user-signup.component.html',
   styleUrl: './user-signup.component.css'
 })
 export class UserSignupComponent {
 
+  // users: {username:string, email:string}[] = [];
+
+  // addUser(username: string, email: string) {
+  //   this.users.push({ username, email });
+  // }
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 }
