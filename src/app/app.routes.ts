d7fb841c50a3a69abbaab93af763f040feb1633a
jmagwili <joshuamagwili@gmail.com>
2024-02-12 12:29:21 +0800

    fixed page routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { UserSignupComponent } from './pages/user-signup/user-signup.component';
+import { PractitionerSignupComponent } from './pages/practitioner-signup/practitioner-signup.component';
+import { FeedComponent } from './pages/feed/feed.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent,
+    },
+    {
+        path: "signin",
+        component: SigninComponent,
+    },
+    {
+        path: "signup/user",
+        component: UserSignupComponent,
+    },
+    {
+        path: "signup/practitioner",
+        component: PractitionerSignupComponent,
+    },
+    {
+        path: "feed",
+        component: FeedComponent,
+    }
+];
