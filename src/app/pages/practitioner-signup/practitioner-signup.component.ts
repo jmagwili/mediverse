4bad3c4fdc37aab98f113b935735af95491af580
jmagwili <joshuamagwili@gmail.com>
2024-02-12 12:30:11 +0800

    created empty components for pages directory

---
+++ src/app/pages/practitioner-signup/practitioner-signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practitioner-signup',
+  standalone: true,
+  imports: [],
+  templateUrl: './practitioner-signup.component.html',
+  styleUrl: './practitioner-signup.component.css'
+})
+export class PractitionerSignupComponent {
+
+}
