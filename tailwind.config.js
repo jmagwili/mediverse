196ba83dcde03957bd6033b8f6beda24441506a6
Puffssss1 <108129228+Puffssss1@users.noreply.github.com>
2024-02-13 00:58:56 +0800

    tailwind custom colors
    global custom colors for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ export const content = [
   "./src/**/*.{html,ts}",
 ];
 export const theme = {
-  extend: {},
+  extend: {
+    colors:{
+      darkgreen: '#018A50',
+      ligthergreen: '#25FFA2',
+      darkblue: '#201939',
+      dirtywhite: '#f5f5f5'
+    }
+  },
 };
 export const plugins = [];
 
