68c0242974506ca1cff3cb64a9b614ff81bd0c96
Puffssss1 <108129228+Puffssss1@users.noreply.github.com>
2024-02-13 15:53:35 +0800

    MED-7_LOGIN_PAGE DONE

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -4,20 +4,33 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatDividerModule, MatFormFieldModule, MatInputModule],
+  imports: 
+  [
+    MatCardModule, 
+    MatButtonModule, 
+    MatDividerModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    RouterLink, 
+    MatIconModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
-  // for now lang inaantok na ako 5 am na po aralin ko pa pano gamitin @input
-  email: string = 'sige';
-  password: string = 'na antok na ako';
+  
+  email: string = '';
+  password: string = '';
 
+  
+  hide = true;
 
   constructor(private router: Router) {}
   
@@ -30,10 +43,4 @@ export class LoginPageComponent {
     //ikaw na bahala jos HAHHAHA
   }
 
-  navigateToSignUp() {
-    this.router.navigate(['/signup/user']);
-  }
-  navitageToFeed(){
-    this.router.navigate(['/feed']);
-  }
 }
--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
+import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatDividerModule],
+  imports: [MatCardModule, MatButtonModule, MatDividerModule, RouterLink],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
 export class SigninComponent {
+
   constructor(private router: Router) {}
 
   signUpWithGoogle() {
@@ -22,12 +24,4 @@ export class SigninComponent {
     //ikaw na bahala jos HAHHAHA
   }
 
-  navigateToSignIn() {
-    
-    this.router.navigate(['/signup/user']);
-  }
-  navitageToLogin(){
-    this.router.navigate(['/login']);
-  }
-  
 }
