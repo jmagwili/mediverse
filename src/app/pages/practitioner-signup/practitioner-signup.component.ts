e5a6e19a2990b2ea6425aee94d0c7107a39f324f
Aileen Olitoquit <108129228+Puffssss1@users.noreply.github.com>
2024-02-13 19:26:19 +0800

    MED-4 - added stepper and pages

--- src/app/pages/practitioner-signup/practitioner-signup.component.ts
+++ src/app/pages/practitioner-signup/practitioner-signup.component.ts
@@ -1,12 +1,196 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';  
+import {FormControl, FormsModule, ReactiveFormsModule, FormBuilder, Validators} from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {Observable} from 'rxjs';
+import {startWith, map} from 'rxjs/operators';
+import {AsyncPipe} from '@angular/common';
+
+export interface StateGroup {
+  letter: string;
+  names: string[];
+}
+
+export const _filter = (opt: string[], value: string): string[] => {
+  const filterValue = value.toLowerCase();
+
+  return opt.filter(item => item.toLowerCase().includes(filterValue));
+};
 
 @Component({
   selector: 'app-practitioner-signup',
   standalone: true,
-  imports: [],
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatStepperModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    AsyncPipe,
+  ],
   templateUrl: './practitioner-signup.component.html',
   styleUrl: './practitioner-signup.component.css'
 })
 export class PractitionerSignupComponent {
+ 
+  myControl = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  thirdFormGroup = this._formBuilder.group({
+    thirdCtrl: ['', Validators.required],
+  });
+  isLinear = false;
 
+  constructor(private _formBuilder: FormBuilder) {}
+
+  // page 3
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  // location picker
+  stateForm = this._formBuilder.group({
+    stateGroup: '',
+  });
+
+  stateGroups: StateGroup[] = [
+    {
+      letter: 'A',
+      names: ['Alabama', 'Alaska', 'Arizona', 'Arkansas'],
+    },
+    {
+      letter: 'C',
+      names: ['California', 'Colorado', 'Connecticut'],
+    },
+    {
+      letter: 'D',
+      names: ['Delaware'],
+    },
+    {
+      letter: 'F',
+      names: ['Florida'],
+    },
+    {
+      letter: 'G',
+      names: ['Georgia'],
+    },
+    {
+      letter: 'H',
+      names: ['Hawaii'],
+    },
+    {
+      letter: 'I',
+      names: ['Idaho', 'Illinois', 'Indiana', 'Iowa'],
+    },
+    {
+      letter: 'K',
+      names: ['Kansas', 'Kentucky'],
+    },
+    {
+      letter: 'L',
+      names: ['Louisiana'],
+    },
+    {
+      letter: 'M',
+      names: [
+        'Maine',
+        'Maryland',
+        'Massachusetts',
+        'Michigan',
+        'Minnesota',
+        'Mississippi',
+        'Missouri',
+        'Montana',
+      ],
+    },
+    {
+      letter: 'N',
+      names: [
+        'Nebraska',
+        'Nevada',
+        'New Hampshire',
+        'New Jersey',
+        'New Mexico',
+        'New York',
+        'North Carolina',
+        'North Dakota',
+      ],
+    },
+    {
+      letter: 'O',
+      names: ['Ohio', 'Oklahoma', 'Oregon'],
+    },
+    {
+      letter: 'P',
+      names: ['Pennsylvania'],
+    },
+    {
+      letter: 'R',
+      names: ['Rhode Island'],
+    },
+    {
+      letter: 'S',
+      names: ['South Carolina', 'South Dakota'],
+    },
+    {
+      letter: 'T',
+      names: ['Tennessee', 'Texas'],
+    },
+    {
+      letter: 'U',
+      names: ['Utah'],
+    },
+    {
+      letter: 'V',
+      names: ['Vermont', 'Virginia'],
+    },
+    {
+      letter: 'W',
+      names: ['Washington', 'West Virginia', 'Wisconsin', 'Wyoming'],
+    },
+  ];
+
+  stateGroupOptions!: Observable<StateGroup[]>;
+
+  
+
+  ngOnInit() {
+    this.stateGroupOptions = this.stateForm.get('stateGroup')!.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filterGroup(value || '')),
+    );
+  }
+
+  private _filterGroup(value: string): StateGroup[] {
+    if (value) {
+      return this.stateGroups
+        .map(group => ({letter: group.letter, names: _filter(group.names, value)}))
+        .filter(group => group.names.length > 0);
+    }
+
+    return this.stateGroups;
+  }
+// 
 }
