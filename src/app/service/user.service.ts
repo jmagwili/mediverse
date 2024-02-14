277040a094f6b7c65e656f64a8d0214e50e3bd49
jmagwili <joshuamagwili@gmail.com>
2024-02-15 00:38:50 +0800

    added function at user service to add new user data to database

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { collection, addDoc } from "firebase/firestore"; 
+import { db } from '../app.config';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +9,18 @@ export class UserService {
 
   constructor(){}
 
+  async addNewUser(data:any){
+    const docRef = await addDoc(collection(db, "users"), {
+      username: data.username,
+      email: data.email,
+      first_name: data.firstName,
+      last_name: data.lastName,
+      suffix: data.suffix,
+      birth_date: data.birthDate,
+      location: data.location,
+      phone: data.phone,
+      interests: data.interests,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  }
 }
