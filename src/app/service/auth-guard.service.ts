2a45d01b9e94d36f9ce91ca176d6957c57711272
jmagwili <joshuamagwili@gmail.com>
2024-02-15 12:10:40 +0800

    added logics to auth guard service

--- src/app/service/auth-guard.service.ts
+++ src/app/service/auth-guard.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuard implements CanActivate {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth, private router: Router) {}
+
+  canActivate(): Observable<boolean> {
+    return this.afAuth.authState.pipe(
+      take(1),
+      map(user => {
+        if (user) {
+          return true; // User is authenticated, allow access
+        } else {
+          this.router.navigate(['/login']); // Redirect to login page if not authenticated
+          return false;
+        }
+      })
+    );
+  }
 }
