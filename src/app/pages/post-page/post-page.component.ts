7565688d80eeb816b41a18c5acf1c7978f6a97fb
Puffssss1 <108129228+Puffssss1@users.noreply.github.com>
2024-02-15 22:40:31 +0800

    MED-13 Post Page
    Created the Post page with visible comments tho styling is not yet finished.

--- src/app/pages/post-page/post-page.component.ts
+++ src/app/pages/post-page/post-page.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 import { PostCardComponent } from '../../components/post-card/post-card.component';
 import { HeaderComponent } from '../../components/header/header.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-post-page',
   standalone: true,
-  imports: [HeaderComponent ,PostCardComponent],
+  imports: [HeaderComponent ,PostCardComponent, MatCardModule, MatButtonModule],
   templateUrl: './post-page.component.html',
   styleUrl: './post-page.component.css'
 })
 export class PostPageComponent {
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
+
+  imageUrl = "../../../assets/images/profile.png";
+
   mockData = [
     {
       id: 1,
       name: "Dr. Cona Grey",
+      tag: "Dentist",
       imageUrl: "../../../assets/images/profile.png",
       comment: "As healthcare professionals, we have a unique perspective on the potential benefits and risks associated with the legalization of marijuana and other drugs. From a medical standpoint, what are your thoughts on this controversial topic?",
       likeCount: 5,
@@ -21,6 +30,7 @@ export class PostPageComponent {
     {
       id: 2,
       name: "Dr. Liu Xin",
+      tag: "Dentist",
       imageUrl: "../../../assets/images/profile.png",
       comment: "It's an intriguing discussion indeed. While some argue that legalization could lead to increased access for medical purposes and potentially reduce criminal activity",
       likeCount: 8,
@@ -28,6 +38,7 @@ export class PostPageComponent {
     {
       id: 3,
       name: "Dr. Maria Dela Cruz",
+      tag: "Dentist",
       imageUrl: "../../../assets/images/profile.png",
       comment: "There's growing evidence suggesting the therapeutic benefits of marijuana, particularly in managing chronic pain, nausea, and certain neurological disorders.",
       likeCount: 3,
@@ -35,6 +46,7 @@ export class PostPageComponent {
     {
       id: 4,
       name: "Dr. Tailor Swift",
+      tag: "Dentist",
       imageUrl: "../../../assets/images/profile.png",
       comment: "it's essential to acknowledge the limitations and uncertainties surrounding its medical use.",
       likeCount: 10,
@@ -42,6 +54,7 @@ export class PostPageComponent {
     {
       id: 5,
       name: "Dr. Gariel Fernandez",
+      tag: "Dentist",
       imageUrl: "../../../assets/images/profile.png",
       comment: "we need stringent regulations in place to ensure safe usage and prevent misuse, especially among vulnerable populations like adolescents.",
       likeCount: 8,
