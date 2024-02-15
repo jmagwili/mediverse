ab3c81c0abd7f0c8bac8db4bcb3720ef28d84955
jmagwili <joshuamagwili@gmail.com>
2024-02-15 14:56:55 +0800

    added mock data for comments at post page

--- src/app/pages/post-page/post-page.component.ts
+++ src/app/pages/post-page/post-page.component.ts
@@ -10,5 +10,37 @@ import { HeaderComponent } from '../../components/header/header.component';
   styleUrl: './post-page.component.css'
 })
 export class PostPageComponent {
+  mockData = [
+    {
+      name: "Dr. Cona Grey",
+      imageUrl: "../../../assets/images/profile.png",
+      comment: "As healthcare professionals, we have a unique perspective on the potential benefits and risks associated with the legalization of marijuana and other drugs. From a medical standpoint, what are your thoughts on this controversial topic?",
+      likeCount: 5,
+    },
+    {
+      name: "Dr. Liu Xin",
+      imageUrl: "../../../assets/images/profile.png",
+      comment: "It's an intriguing discussion indeed. While some argue that legalization could lead to increased access for medical purposes and potentially reduce criminal activity",
+      likeCount: 8,
+    },
+    {
+      name: "Dr. Maria Dela Cruz",
+      imageUrl: "../../../assets/images/profile.png",
+      comment: "There's growing evidence suggesting the therapeutic benefits of marijuana, particularly in managing chronic pain, nausea, and certain neurological disorders.",
+      likeCount: 3,
+    },
+    {
+      name: "Dr. Tailor Swift",
+      imageUrl: "../../../assets/images/profile.png",
+      comment: "it's essential to acknowledge the limitations and uncertainties surrounding its medical use.",
+      likeCount: 10,
+    },
+    {
+      name: "Dr. Gariel Fernandez",
+      imageUrl: "../../../assets/images/profile.png",
+      comment: "we need stringent regulations in place to ensure safe usage and prevent misuse, especially among vulnerable populations like adolescents.",
+      likeCount: 8,
+    },
 
+  ]
 }
