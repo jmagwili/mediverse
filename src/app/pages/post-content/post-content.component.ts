34cb9a127a162c7e1782a65a6dbc95c502ac536f
m4rceee <91464427+m4rceee@users.noreply.github.com>
2024-02-15 22:44:04 +0800

    added the postType button

--- src/app/pages/post-content/post-content.component.ts
+++ src/app/pages/post-content/post-content.component.ts
@@ -3,6 +3,9 @@ import { HeaderComponent } from '../../components/header/header.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatChipsModule} from '@angular/material/chips';
+import { MatChipListbox } from '@angular/material/chips';
 
 @Component({
   selector: 'app-post-content',
@@ -12,11 +15,14 @@ import {MatCardModule} from '@angular/material/card';
     MatButtonModule,
     MatIconModule,
     MatCardModule,
+    MatMenuModule,
+    MatChipsModule,
   ],
   templateUrl: './post-content.component.html',
   styleUrl: './post-content.component.css'
 })
 export class PostContentComponent {
+
   structuredData = {
     postType:"public",
     name: "Dr. John",
