71ce4e033562b96fbf19feffc8592b94faba905f
m4rceee <91464427+m4rceee@users.noreply.github.com>
2024-02-15 22:22:03 +0800

    added post-button, post-card (w header), and some style to the post-content component

--- src/app/pages/post-content/post-content.component.ts
+++ src/app/pages/post-content/post-content.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-post-content',
   standalone: true,
   imports: [
     HeaderComponent,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
   ],
   templateUrl: './post-content.component.html',
   styleUrl: './post-content.component.css'
@@ -18,4 +24,6 @@ export class PostContentComponent {
     content: "I like hotdogs",
     category: ["dentistry","pediatrics"]
   }
+
+  sampleImage = "../../../assets/images/profile.png";
 }
