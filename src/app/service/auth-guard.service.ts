1679d034fc11eeeef6d11ce8b4318dce746a776a
jmagwili <joshuamagwili@gmail.com>
2024-02-15 15:40:38 +0800

    updated auth guard logic

--- src/app/service/auth-guard.service.ts
+++ src/app/service/auth-guard.service.ts
@@ -3,21 +3,22 @@ import { Router } from '@angular/router';
 import { ActivatedRouteSnapshot } from '@angular/router';
 import { RouterStateSnapshot } from '@angular/router';
 import { CanActivateFn } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class AuthGuard {
+  isLoggedIn
 
-  constructor( private router: Router) {}
-    canActivate() {
-      return true;
-    }
-    canMatch(): boolean {
-      return false;
-    }
+  constructor( private router: Router, private authService: AuthService) {
+    this.isLoggedIn = authService.isLoggedIn
+  }
 
+  canActivate() {
+    return this.isLoggedIn() ? true : this.router.navigate(["/"])
+  }
 }
 
 export const canActivateTeam: CanActivateFn = (
