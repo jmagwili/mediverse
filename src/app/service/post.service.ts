7f1451198c66c7f2e66844bbac1980a74f9ef8a4
jmagwili <joshuamagwili@gmail.com>
2024-02-16 16:40:09 +0800

    added method at post service to add public post

--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { collection, addDoc } from "firebase/firestore"; 
+import { db } from '../app.config';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,21 @@ import { Injectable } from '@angular/core';
 export class PostService {
 
   constructor() { }
+
+  async addPublicPost(data:any){
+    const docRef = await addDoc(collection(db, "posts"), {
+      email: data.email,
+      first_name: data.firstName,
+      profile_image: data.profileImage,
+      like_count: 0,
+      likes:[],
+      image_upload:[],
+      comment_count: 0,
+      comments:[],
+      repost_count: 0,
+      reposts:[],
+    });
+    console.log("Document written with ID: ", docRef.id);
+  }
+
 }
