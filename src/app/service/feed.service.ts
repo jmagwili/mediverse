4ac6ba9c387626d5a88e6f4a597cab8b55928c16
jmagwili <joshuamagwili@gmail.com>
2024-02-16 23:07:25 +0800

    updated logic to getForYouFeed at feed service

--- src/app/service/feed.service.ts
+++ src/app/service/feed.service.ts
@@ -1,42 +1,73 @@
 import { Injectable } from '@angular/core';
-import { collection, query, where, getDocs, limit } from "firebase/firestore";
+import { collection, query, where, getDocs, limit, QueryDocumentSnapshot } from "firebase/firestore";
 import { db } from '../app.config';
 
 @Injectable({
   providedIn: 'root'
 })
+
+interface User {
+  interests?: string[]; // Define the properties of the user object
+  following?: string[];
+}
+
 export class FeedService {
 
   constructor() { }
 
   async getForYouFeed(email:string){
-    let user: object | null = null;
-    
-    const userQuery = query(collection(db, "users"), 
-    where("email", "==", email),
+    let user: User | null = null; // Explicitly define the type of user
+
+    const userQuery = query(
+        collection(db, "users"), 
+        where("email", "==", email)
     );
+
     const querySnapshot = await getDocs(userQuery);
-    
+
+    let userData: any; // Define a variable to capture user data
+
     await Promise.all(querySnapshot.docs.map(async (doc) => {
-      user = doc.data();
-  }));
+        userData = doc.data();
+    }));
 
-    console.log(user)
+    user = userData as User; // Assign the user data to the user variable
 
-    // const q1 = query(collection(db, "posts"), 
-    //   where("category", "in", interests),
-    //   limit(10)  
-    // );
+    if (user) { // Check if user exist
+        const q1 = query(
+            collection(db, "posts"), 
+            where("category", "in", user.interests),
+            limit(10)  
+        );
 
-    // const q2 = query(collection(db, "posts"), 
-    // where("email", "in", interests),
-    // limit(10)  
-    // );
+        const q2 = query(collection(db, "posts"), 
+          where("email", "in", user.following),
+          limit(10)  
+        );
 
-    // const querySnapshot = await getDocs(q1);
-    // querySnapshot.forEach((doc) => {
-    // // doc.data() is never undefined for query doc snapshots
-    // console.log(doc.id, " => ", doc.data());
-    // });
+        const [querySnapshot1, querySnapshot2] = await Promise.all([
+          getDocs(q1),
+          getDocs(q2),
+      ]);
+
+      const mergedResults: QueryDocumentSnapshot[] = [
+        ...querySnapshot1.docs,
+        ...querySnapshot2.docs
+      ];
+
+      const shuffledResults = this.shuffleArray(mergedResults)
+
+      shuffledResults.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+    });
+    }
   }
+
+  shuffleArray(array: any[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 }
