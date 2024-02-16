e5c00755d189cd09446be68c5e2dca981485309e
jmagwili <joshuamagwili@gmail.com>
2024-02-16 20:07:05 +0800

    added logic at feed service to fetch user data

--- src/app/service/feed.service.ts
+++ src/app/service/feed.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { collection, query, where, getDocs, limit } from "firebase/firestore";
+import { db } from '../app.config';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,35 @@ import { Injectable } from '@angular/core';
 export class FeedService {
 
   constructor() { }
+
+  async getForYouFeed(email:string){
+    let user: object | null = null;
+    
+    const userQuery = query(collection(db, "users"), 
+    where("email", "==", email),
+    );
+    const querySnapshot = await getDocs(userQuery);
+    
+    await Promise.all(querySnapshot.docs.map(async (doc) => {
+      user = doc.data();
+  }));
+
+    console.log(user)
+
+    // const q1 = query(collection(db, "posts"), 
+    //   where("category", "in", interests),
+    //   limit(10)  
+    // );
+
+    // const q2 = query(collection(db, "posts"), 
+    // where("email", "in", interests),
+    // limit(10)  
+    // );
+
+    // const querySnapshot = await getDocs(q1);
+    // querySnapshot.forEach((doc) => {
+    // // doc.data() is never undefined for query doc snapshots
+    // console.log(doc.id, " => ", doc.data());
+    // });
+  }
 }
