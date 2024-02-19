e1d1e4499a6c2b9db55c40e0c70e57a66a7020b6
jmagwili <joshuamagwili@gmail.com>
2024-02-19 22:45:48 +0800

    updated logics at getForYouFeed method at feed service

--- src/app/service/feed.service.ts
+++ src/app/service/feed.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { collection, query, where, getDocs, limit, QueryDocumentSnapshot } from "firebase/firestore";
 import { db } from '../app.config';
 
-@Injectable({
-  providedIn: 'root'
-})
-
 interface User {
   interests?: string[]; // Define the properties of the user object
   following?: string[];
 }
+
+@Injectable({
+  providedIn: 'root'
+})
 
 export class FeedService {
 
@@ -33,21 +33,22 @@ export class FeedService {
 
     user = userData as User; // Assign the user data to the user variable
 
-    if (user) { // Check if user exist
-        const q1 = query(
-            collection(db, "posts"), 
-            where("category", "in", user.interests),
-            limit(10)  
-        );
 
-        const q2 = query(collection(db, "posts"), 
-          where("email", "in", user.following),
-          limit(10)  
-        );
+    if(user && user.following && user.following){
+      const q1 = query(
+        collection(db, "posts"), 
+        where("category", "in", user.interests),
+        limit(10)  
+      );
 
-        const [querySnapshot1, querySnapshot2] = await Promise.all([
-          getDocs(q1),
-          getDocs(q2),
+      const q2 = query(collection(db, "posts"), 
+        where("email", "in", user.following),
+        limit(10)  
+      );
+
+      const [querySnapshot1, querySnapshot2] = await Promise.all([
+        getDocs(q1),
+        getDocs(q2),
       ]);
 
       const mergedResults: QueryDocumentSnapshot[] = [
@@ -59,7 +60,24 @@ export class FeedService {
 
       shuffledResults.forEach((doc) => {
         console.log(doc.id, " => ", doc.data());
-    });
+      });
+
+    }else if(user && user.interests){
+      const q1 = query(
+        collection(db, "posts"), 
+        where("category", "array-contains-any", user.interests),
+        limit(10)  
+      );
+
+      const querySnapshot = await getDocs(q1)
+      
+      const post:Array<object> = []
+
+      querySnapshot.forEach((doc)=>{
+        post.push(doc.data())
+      })
+      
+      console.log(post)
     }
   }
 
