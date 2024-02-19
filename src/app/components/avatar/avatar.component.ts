84ffac5d658fd10e248f5b0a62e596fb8b420cfe
m4rceee <91464427+m4rceee@users.noreply.github.com>
2024-02-20 00:19:53 +0800

    modified the avatar input styling

--- src/app/components/avatar/avatar.component.ts
+++ src/app/components/avatar/avatar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
@@ -6,10 +7,28 @@ import { MatIcon } from '@angular/material/icon';
   standalone: true,
   imports: [
     MatIcon,
+    CommonModule,
   ],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.css'
 })
 export class AvatarComponent {
+  file: string = '';
 
+  onFileChange(event: any) {
+    const files = event.target.files as FileList;
+
+    if (files.length > 0) {
+      const _file = URL.createObjectURL(files[0]);
+      this.file = _file;
+      this.resetInput();   
+    }
+  }
+
+ resetInput(){
+    const input = document.getElementById('avatar-input-file') as HTMLInputElement;
+    if(input){
+      input.value = "";
+    }
+  }
 }
