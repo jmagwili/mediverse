0205f98ef1ba0740d6c40fb6fa126e2489910800
jmagwili <joshuamagwili@gmail.com>
2024-02-19 23:16:55 +0800

    added logic to unlikePost method at post service

--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -50,9 +50,29 @@ export class PostService {
     }
   }
 
-  async unLikePost(data:any){
-
+  async unlikePost(data: any) {
+    const postRef = doc(db, "posts", data.postID);
+  
+    // Fetch the current document data
+    const postSnap = await getDoc(postRef);
+    if (postSnap.exists()) {
+      const postData = postSnap.data();
+      const currentLikeCount = postData["like_count"]; // Ensure like_count exists and handle potential null value
+  
+      // Increment the like_count value
+      const updatedLikeCount = currentLikeCount - 1;
+  
+      // Update the document with the new like_count value
+      await updateDoc(postRef, {
+        like_count: updatedLikeCount
+      });
+  
+      console.log("Like count decremented successfully.");
+    } else {
+      console.log("Document does not exist.");
+    }
   }
+
 
   async addPublicComment(data:any){
 
