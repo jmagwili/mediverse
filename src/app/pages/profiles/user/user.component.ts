2e2d02d133383226fe2297b74ac39a34d63da8fa
m4rceee <91464427+m4rceee@users.noreply.github.com>
2024-02-21 01:25:53 +0800

    added the profile-title-info section

--- src/app/pages/profiles/user/user.component.ts
+++ src/app/pages/profiles/user/user.component.ts
@@ -3,12 +3,17 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../../components/header/header.component';
 import { AvatarComponent } from '../../../components/avatar/avatar.component';
 
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [
     HeaderComponent,
     AvatarComponent,
+    MatIconModule,
+    MatButtonModule,
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
@@ -17,10 +22,10 @@ export class UserComponent {
 
   userProfile = {
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    userName: '',
-    birthday: '',
-    posts: '',
-    followers: '', 
+    userName: 'Marc Justine',
+    birthday: 'July 17, 2002',
+    postCount: '12',
+    followerCount: '696', 
     isFollowed: false,
   }
 }
