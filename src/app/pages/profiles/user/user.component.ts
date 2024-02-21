a8fe571731699184de54d431066932e9e48e0419
m4rceee <91464427+m4rceee@users.noreply.github.com>
2024-02-21 13:26:47 +0800

    modified follow logic

--- src/app/pages/profiles/user/user.component.ts
+++ src/app/pages/profiles/user/user.component.ts
@@ -25,15 +25,27 @@ export class UserComponent {
   userProfile = {
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
     userName: 'Marc Justine',
-    birthday: 'July 17, 2002',
+    dateJoined: {
+      month: 'January',
+      day: '13',
+      year: '2013'
+  },
     postCount: '12',
-    followerCount: '696', 
+    followerCount: 696, 
     isFollowed: false,
   }
 
   toggleFollow() {
-    this.userProfile.isFollowed = !this.userProfile.isFollowed;
-    console.log(this.userProfile.isFollowed)
+    if (this.userProfile.isFollowed) {
+      // If currently followed, unfollow and decrement follower count
+      this.userProfile.isFollowed = false;
+      this.userProfile.followerCount--;
+    } else {
+      // If currently not followed, follow and increment follower count
+      this.userProfile.isFollowed = true;
+      this.userProfile.followerCount++;
+    }
+    console.log("User follow status: " + this.userProfile.isFollowed);
   }
-  
+
 }
