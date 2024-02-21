f720c925f69b15ebb20eb15ad3e20f73f0387420
m4rceee <91464427+m4rceee@users.noreply.github.com>
2024-02-21 19:51:12 +0800

    modified avatar and header image styling

--- src/app/pages/profiles/practitioner/practitioner.component.ts
+++ src/app/pages/profiles/practitioner/practitioner.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { AvatarComponent } from '../../../components/avatar/avatar.component';
 import { HeaderImageComponent } from '../../../components/header-image/header-image.component';
-
 import { HeaderComponent } from '../../../components/header/header.component';
+
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-practitioner',
@@ -11,10 +13,40 @@ import { HeaderComponent } from '../../../components/header/header.component';
     HeaderComponent,
     AvatarComponent,
     HeaderImageComponent,
+    MatButton,
+    MatIcon
   ],
   templateUrl: './practitioner.component.html',
   styleUrl: './practitioner.component.css'
 })
 export class PractitionerComponent {
+
+  userProfile = {
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    userName: 'Marc Justine',
+    tag: "Practitioner",
+    dateJoined: {
+      month: 'January',
+      day: '13',
+      year: '2013'
+    },
+    postCount: '12',
+    followerCount: 696, 
+    isFollowed: false,
+  }
+
+  toggleFollow() {
+    if (this.userProfile.isFollowed) {
+      // If currently followed, unfollow and decrement follower count
+      this.userProfile.isFollowed = false;
+      this.userProfile.followerCount--;
+    } else {
+      // If currently not followed, follow and increment follower count
+      this.userProfile.isFollowed = true;
+      this.userProfile.followerCount++;
+    }
+    console.log("User follow status: " + this.userProfile.isFollowed);
+    console.log("New follower count: " + this.userProfile.followerCount);
+  }
 
 }
