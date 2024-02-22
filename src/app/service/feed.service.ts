2b8e2308974ab00fc45d5fab640b81bd8ea6a878
jmagwili <joshuamagwili@gmail.com>
2024-02-22 11:47:30 +0800

    updated the logic of getForYouFeed at feed service

--- src/app/service/feed.service.ts
+++ src/app/service/feed.service.ts
@@ -16,78 +16,6 @@ export class FeedService {
 
   constructor() { }
 
-  // async getForYouFeed(email:string){
-  //   let user: User | null = null; // Explicitly define the type of user
-  //   const post:any = []
-
-  //   const userQuery = query(
-  //       collection(db, "users"), 
-  //       where("email", "==", email)
-  //   );
-
-  //   const querySnapshot = await getDocs(userQuery);
-
-  //   let userData: any; // Define a variable to capture user data
-
-  //   await Promise.all(querySnapshot.docs.map(async (doc) => {
-  //       userData = doc.data();
-  //   }));
-
-  //   user = userData as User; // Assign the user data to the user variable
-
-
-  //   if(user && user.following && user.following.length > 0 && user.interests && user.location){
-  //     const q1 = query(
-  //       collection(db, "posts"), 
-  //       where("category", "array-contains-any", user.interests),
-  //       limit(10)  
-  //     );
-
-  //     const q2 = query(collection(db, "posts"), 
-  //       where("email", "array-contains-any", user.following),
-  //       limit(10)  
-  //     );
-
-  //     const [querySnapshot1, querySnapshot2] = await Promise.all([
-  //       getDocs(q1),
-  //       getDocs(q2),
-  //     ]);
-
-  //     const mergedResults: QueryDocumentSnapshot[] = [
-  //       ...querySnapshot1.docs,
-  //       ...querySnapshot2.docs
-  //     ];
-
-  //     const shuffledResults = this.shuffleArray(mergedResults)
-
-  //     shuffledResults.forEach((doc) => {
-  //       console.log(doc.id, " => ", doc.data());
-  //     });
-
-  //   }else if(user && user.interests){
-      
-  //     console.log(user.interests)
-
-  //     const q1 = query(
-  //       collection(db, "posts"), 
-  //       where("category", "array-contains-any", user.interests),
-  //       limit(10)  
-  //     );
-
-  //     const querySnapshot = await getDocs(q1)
-     
-  //     let index = 0
-  //     querySnapshot.forEach((doc)=>{
-  //       post.push(doc.data())
-  //       post[index].id = doc.id
-  //       index++
-  //     })
-      
-  //     // console.log(post)
-  //   }
-  //   return post
-  // }
-
   async getForYouFeed(email:string){
     let user: User | null = null; // Explicitly define the type of user
     let userData: any;
@@ -107,15 +35,21 @@ export class FeedService {
       const lowerCaseInterest = user.interests.map(interest => interest.toLowerCase());
 
       const q= query(collection(db,"posts"),
-      or( where("category", "array-contains-any", lowerCaseInterest),
-          where("user_location", "==", user.location),
-          where("email", "in", user.following),
-        ),limit(20)
-      )
-
+        or( where("category", "array-contains-any", lowerCaseInterest),
+            where("user_location", "==", user.location),
+            where("email", "in", user.following),
+          ),limit(20)
+        )
+      
+      const querySnapshot = await getDocs(q)
+      querySnapshot.forEach((doc)=>{
+        post.push({
+          ...doc.data(),
+          id: doc.id
+        })
+      })
 
     }else if(user && user.interests){
-
       const lowerCaseInterest = user.interests.map(interest => interest.toLowerCase());
 
       const q= query(collection(db,"posts"),
@@ -126,13 +60,46 @@ export class FeedService {
       
       const querySnapshot = await getDocs(q)
       
-      querySnapshot.forEach((doc)=>post.push(doc.data()))
+      querySnapshot.forEach((doc)=>{
+        post.push({
+          ...doc.data(),
+          id: doc.id,
+        })
+
+      })
+    }
+
+    if(post.length < 10){
+      
+      if(post.length > 0){
+        const IDList = post.map((data:any)=>data.id)
+        console.log(IDList)
+  
+        const q = query(collection(db,"posts"),where("id", "not-in", IDList), limit(15))
+  
+        const querySnapshot = await getDocs(q)
+  
+        querySnapshot.forEach((doc)=>{
+          post.push({
+            ...doc.data(),
+            id: doc.id
+          })
+        })
+
+      }else{
+        const q = query(collection(db, "posts"), limit(15))
+        const querySnapshot = await getDocs(q)
+        querySnapshot.forEach((doc)=>{
+          post.push({
+            ...doc.data(),
+            id: doc.id
+          })
+        })
+      }
     }
     
     return this.shuffleArray(post)
   }
-
-
 
   shuffleArray(array: any[]) {
     for (let i = array.length - 1; i > 0; i--) {
