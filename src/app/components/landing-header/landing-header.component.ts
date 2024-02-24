f7e660965bbd09282adc2f279b4cfd0c3e0ec2ba
m4rceee <91464427+m4rceee@users.noreply.github.com>
2024-02-24 16:10:35 +0800

    modified landing page header styling

--- src/app/components/landing-header/landing-header.component.ts
+++ src/app/components/landing-header/landing-header.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+
 @Component({
   selector: 'app-landing-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIcon,
+  ],
   templateUrl: './landing-header.component.html',
   styleUrl: './landing-header.component.css'
 })
 export class LandingHeaderComponent {
+
+  title: string = 'MediVerse';
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import { LandingHeaderComponent } from '../../components/landing-header/landing-header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatSlideToggleModule],
+  imports: [
+    LandingHeaderComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
