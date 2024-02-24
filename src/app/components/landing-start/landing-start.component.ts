6cf8982a3f91846b0d0e27d1242c19111cbe50cd
m4rceee <91464427+m4rceee@users.noreply.github.com>
2024-02-25 02:16:14 +0800

    complete landing-start component

--- src/app/components/landing-start/landing-start.component.ts
+++ src/app/components/landing-start/landing-start.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+import { MatIcon } from '@angular/material/icon';
+
 @Component({
   selector: 'app-landing-start',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon
+  ],
   templateUrl: './landing-start.component.html',
   styleUrl: './landing-start.component.css'
 })
