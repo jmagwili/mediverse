121b73630d383dc2fad316b912627fd8268131ca
m4rceee <91464427+m4rceee@users.noreply.github.com>
2024-02-24 23:46:58 +0800

    imported mat-card

--- src/app/components/landing-community/landing-community.component.ts
+++ src/app/components/landing-community/landing-community.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
 import {MatGridListModule} from '@angular/material/grid-list';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'app-landing-community',
   standalone: true,
   imports: [
     MatGridListModule,
+    MatCard,
   ],
   templateUrl: './landing-community.component.html',
   styleUrl: './landing-community.component.css'
