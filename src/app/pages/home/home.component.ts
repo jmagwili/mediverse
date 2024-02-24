049d6fd020a700559dc7c268a3d03261dc894eae
m4rceee <91464427+m4rceee@users.noreply.github.com>
2024-02-24 17:24:00 +0800

    finished landing-home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
 import { LandingHeaderComponent } from '../../components/landing-header/landing-header.component';
+import { LandingHomeComponent } from '../../components/landing-home/landing-home.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     LandingHeaderComponent,
+    LandingHomeComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
