efbcd4df845c3fb591d8259e414b8bca70c7b174
jmagwili <joshuamagwili@gmail.com>
2024-02-25 13:37:18 +0800

    added click handler for name click at post card component

--- src/app/components/post-card/post-card.component.ts
+++ src/app/components/post-card/post-card.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import { ReactionButtonComponent } from '../reaction-button/reaction-button.component';
+import { UserService } from '../../service/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-card',
@@ -18,6 +20,12 @@ import { ReactionButtonComponent } from '../reaction-button/reaction-button.comp
 export class PostCardComponent {
 
 @Input() data:any = {};
+
+constructor(private userService:UserService, private router:Router){}
+
+async nameClick(){
+  this.router.navigate([`/profile/user/${this.data.email}`])
+}
 
 imageUrl = "../../../assets/images/profile.png";
 }
